b3dba885607013c72ced66c9289ad4f538ed1066
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2026-03-28 19:53:18 +0300

    fixed locCard

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,23 +1,14 @@
 import LocationCard from "@/src/components/cards/LocationCard/LocationCard";
-import MaxWidthWrapper from "@/src/components/ui/MaxWidthWrapper";
 import { LOCATIONS } from "@/src/components/cards/LocationCard/constants";
 import { Locationtype } from "@/src/components/cards/LocationCard/types";
+import MaxWidthWrapper from "@/src/components/ui/MaxWidthWrapper";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-50">
-			<MaxWidthWrapper className="max-w-420">
-				<div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto py-1">
-					{LOCATIONS.map((loc) => (
-						<LocationCard
-							key={loc.id}
-							location={loc}
-						/>
-					))}
-				</div>
-			</MaxWidthWrapper>
-		</main>
-
-
-	);
+    return (
+        <main>
+            <MaxWidthWrapper>
+                <LocationCard />
+            </MaxWidthWrapper>
+        </main>
+    );
 }
--- frontend/src/components/cards/LocationCard/LocationCard.tsx
+++ frontend/src/components/cards/LocationCard/LocationCard.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import { LOCATIONS } from "./constants";
 import { Locationtype } from "./types";
+import MaxWidthWrapper from "../../ui/MaxWidthWrapper";
 
-type LocationCardProps = { location: Locationtype };
-
-export default function LocationCard({ location }: LocationCardProps) {
+export default function LocationCard() {
     return (
-        <div className="flex flex-col w-full mx-auto bg-white shadow-xs hover:shadow-md rounded-xl cursor-pointer transition-all duration-300 hover:scale-110 max-w-420">
-            <div className="h-30 rounded-xl border-2 border-gray-400 flex flex-col gap-1 px-5 p-4 text-center">
-                <h3 className="ext-lg font-bold text-gray-500">{location.adressName}</h3>
-                <Link
-                    href={location.adressLink}
-                    className="text-gray-500 mt-1 hover:text-gray-400 text-sm">
-                    {location.exactLocName}
-                </Link>
+        <MaxWidthWrapper>
+            <div className="grid grid-cols-1 min-h-screen items-center md:grid-cols-3 gap-6 mx-auto py-1 bg-white ">
+                {LOCATIONS.map((location) => (
+                    <div className="h-30 rounded-xl border-2 border-gray-400 flex flex-col justify-center text-center cursor-pointer shadow-xs hover:shadow-md transition-all duration-300 hover:scale-105">
+                        <h3 className="text-lg font-bold text-gray-500">
+                            {location.adressName}
+                        </h3>
+                        <Link
+                            href={location.adressLink}
+                            className="text-gray-500 mt-1 hover:text-gray-400 text-sm"
+                        >
+                            {location.exactLocName}
+                        </Link>
+                    </div>
+                ))}
             </div>
-        </div>
+        </MaxWidthWrapper>
     );
 }
