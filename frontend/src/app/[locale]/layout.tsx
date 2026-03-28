2b60273fc56e25e2b8ac495603e4b8ec34ebed72
Parsa <parsaxavier@gmail.com>
2026-03-28 21:43:26 +0300

    merging from sait/feat/footer #2

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -3,29 +3,21 @@ import { getMessages, getLocale } from "next-intl/server";
 import Navbar from "@/src/components/ui/navbar";
 
 export default async function LocaleLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    const locale = await getLocale();
-    const messages = await getMessages();
+	const locale = await getLocale();
+	const messages = await getMessages();
 
-<<<<<<< HEAD
-	if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
-		notFound();
-	}
-
-	return children;
-=======
-    return (
-        <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
-            <body>
-                <NextIntlClientProvider locale={locale} messages={messages}>
-                    <Navbar />
-                    {children}
-                </NextIntlClientProvider>
-            </body>
-        </html>
-    );
->>>>>>> 92f71132be011e3c16432dd5d75c01c879542395
+	return (
+		<html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+			<body>
+				<NextIntlClientProvider locale={locale} messages={messages}>
+					<Navbar />
+					{children}
+				</NextIntlClientProvider>
+			</body>
+		</html>
+	);
 }
