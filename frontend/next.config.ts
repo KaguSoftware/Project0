52ba8e591781e08c589d030e7eebd2d6d8e486af
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2026-03-31 20:25:57 +0300

    fixed big aaaahhh error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,30 +1,12 @@
-// next.config.ts
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "127.0.0.1",
-				port: "1337",
-				pathname: "/**",
-			},
-			{
-				protocol: "http",
-				hostname: "localhost",
-				port: "1337",
-				pathname: "/**",
-			},
-		],
-	},
-	experimental: {
-		turbopackFileSystemCacheForDev:
-			process.env.DISABLE_TURBO_CACHE !== "true",
-	},
+    turbopack: {
+        root: __dirname,
+    },
 };
 
 export default withNextIntl(nextConfig);
