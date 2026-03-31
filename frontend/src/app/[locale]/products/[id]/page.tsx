7a8e5fd692154a4f799aaa4288ccc3a5cff51dc3
Parsa <parsaxavier@gmail.com>
2026-03-31 15:41:36 +0300

    categories dynamic routing

--- frontend/src/app/[locale]/products/[id]/page.tsx
+++ frontend/src/app/[locale]/products/[id]/page.tsx
@@ -74,7 +74,7 @@ export default async function ProductDetail({
 									src={getMediaUrl(img.url)}
 									width={75}
 									height={75}
-									className="object-cover rounded-xl w-[75px] h-[75px]"
+									className="object-cover rounded-xl w-18.75 h-18.75"
 								/>
 							))}
 						</div>
